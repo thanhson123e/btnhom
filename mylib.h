7d2bcb8891bd51a34e37ae1f2fc54dc550f0129c
ThanhSon <nguyenthanhsonmwg123@gmail.com>
2025-10-21 10:32:58 +0700

    lan4

---
+++ mylib.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void soNgayCuaThang();
+int namNhuan();
+void giaiPhuongTrinhBac2();
+float tinhDtb();
+long tinhTienDien();
+void bangCuuChuong();
+int giaiThua();
+void inTamGiacSo();
+void nhapDungDenN();
+void fibonacci();
+void inTamGiacSao();
